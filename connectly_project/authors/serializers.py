3fa9d396e9eefb8817290526c3a92c64ea7e0760
newb3 <neitsajsarip@gmail.com>
2026-02-13 06:26:36 -0500

    Add CRUD updates for authors

--- connectly_project/authors/serializers.py
+++ connectly_project/authors/serializers.py
@@ -1,7 +1,17 @@
 from rest_framework import serializers
-from .models import Author
+from .models import User, Post, Comment
 
-class AuthorSerializer(serializers.ModelSerializer):
+class UserSerializer(serializers.ModelSerializer):
     class Meta:
-        model = Author
+        model = User
+        fields = '__all__'
+
+class PostSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Post
+        fields = '__all__'
+
+class CommentSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Comment
         fields = '__all__'
